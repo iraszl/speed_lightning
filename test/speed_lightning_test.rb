af6cb9f059265400045135c57d0bd558f32ec558
Ivan Raszl <iraszl@gmail.com>
2023-10-22 18:23:46 +0800

    preparing for tests

--- test/speed_lightning_test.rb
+++ test/speed_lightning_test.rb
@@ -1,13 +1,11 @@
-# frozen_string_literal: true
-
-require "test_helper"
+require_relative "test_helper"
 
 class SpeedLightningTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::SpeedLightning::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
+  # def test_it_does_something_useful
+  #   assert false
+  # end
 end
